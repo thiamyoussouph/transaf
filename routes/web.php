13e8121afdfb75aa99d2f8d449ed8cc73b35ce36
Antaal <antambaye261971@gmail.com>
2023-01-03 17:35:38 +0000

    Add Template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
+ use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,16 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Home page routes
+Route::get('/home', function () {
+    return view('home');
+});
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/camion', [App\Http\Controllers\CamionController::class, 'index'])->name('camion.index');
-Route::get('/camion/create', [App\Http\Controllers\CamionController::class, 'create'])->name('camion.forme');
-Route::post('/add', [App\Http\Controllers\CamionController::class, 'store'])->name('camion.store');
-Route::get('/camion/{id}', [App\Http\Controllers\CamionController::class, 'show'])->name('camion.detail');
-Route::get('/lieu', [App\Http\Controllers\LieuController::class, 'index'])->name('lieu.index');
-Route::get('/lieu/create', [App\Http\Controllers\LieuController::class, 'create'])->name('lieu.forme');
-Route::post('/add', [App\Http\Controllers\LieuController::class, 'store'])->name('lieu.store');
-Route::get('/categorie', [App\Http\Controllers\CategorieController::class, 'index'])->name('categorie.index');
-Route::get('/categorie/create', [App\Http\Controllers\CategorieController::class, 'create'])->name('categorie.forme');
-Route::post('/add', [App\Http\Controllers\CategorieController::class, 'store'])->name('categorie.store');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/camion', [App\Http\Controllers\CamionController::class, 'index'])->name('camion.index');
+// Route::get('/camion/create', [App\Http\Controllers\CamionController::class, 'create'])->name('camion.forme');
+// Route::post('/add', [App\Http\Controllers\CamionController::class, 'store'])->name('camion.store');
+// Route::get('/camion/{id}', [App\Http\Controllers\CamionController::class, 'show'])->name('camion.detail');
+// Route::get('/lieu', [App\Http\Controllers\LieuController::class, 'index'])->name('lieu.index');
+// Route::get('/lieu/create', [App\Http\Controllers\LieuController::class, 'create'])->name('lieu.forme');
+// Route::post('/add', [App\Http\Controllers\LieuController::class, 'store'])->name('lieu.store');
+// Route::get('/categorie', [App\Http\Controllers\CategorieController::class, 'index'])->name('categorie.index');
+// Route::get('/categorie/create', [App\Http\Controllers\CategorieController::class, 'create'])->name('categorie.forme');
+// Route::post('/add', [App\Http\Controllers\CategorieController::class, 'store'])->name('categorie.store');
+
+
+
+
