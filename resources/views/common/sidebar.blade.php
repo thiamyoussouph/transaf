7ce2eb1ca2e0c4739b4b88b002d5be2611a8030c
unknown <aichafatybittyba@gmail.com>
2023-01-06 14:01:51 +0000

    update gestion comions

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -58,6 +58,22 @@
         </div>
     </li>
 
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Lieux</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Gestion Lieux:</h6>
+                <a href="#" class="collapse-item" >Lister</a>
+                <a href="#" class="collapse-item" >Ajout</a>
+            </div>
+        </div>
+    </li>
+
     <!-- Divider -->
     <hr class="sidebar-divider">
 
