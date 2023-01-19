34e6fb61def518dce637ef5fb87a4b42ff78d5ba
youssouph <thiamasjo@gmail.com>
2023-01-19 12:56:18 +0000

    gestion dechargement

--- app/Models/chargement.php
+++ app/Models/chargement.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class chargement extends Model
+class Chargement extends Model
 {
     use HasFactory;
     protected $fillable = [
